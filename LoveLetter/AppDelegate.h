9bb6844be393ca3e07e2da05fc377209c5aa52ee
user <user@users-MacBook-Air.local>
2013-01-18 19:05:35 -0600

    Initial commit.  Mmm, mmm: File -> New -> Project...

---
+++ LoveLetter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LoveLetter
+//
+//  Created by user on 1/18/13.
+//  Copyright (c) 2013 Kevin Lawson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LoveLetter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LoveLetter
+//
+//  Created by user on 1/18/13.
+//  Copyright (c) 2013 Kevin Lawson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
