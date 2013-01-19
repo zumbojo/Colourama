5e828b8d36b56f8b2399b10de4274d9bbf96340a
user <user@users-MacBook-Air.local>
2013-01-18 20:40:44 -0600

    Starting on slat views.

--- LoveLetter/CLPaletteView.m
+++ LoveLetter/CLPaletteView.m
@@ -20,7 +20,11 @@
         
         NSUInteger randomIndex = arc4random_uniform(self.palette.slats.count);
         CLSlat *slat = ((CLSlat *)self.palette.slats[randomIndex]);
-        self.layer.backgroundColor = slat.color.CGColor;
+//        self.layer.backgroundColor = slat.color.CGColor;
+        
+        UIView *slatView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width / 5, self.frame.size.height)];
+        slatView.layer.backgroundColor = slat.color.CGColor;
+        [self addSubview:slatView];
     }
     return self;
 }
