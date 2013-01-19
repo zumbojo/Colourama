b8d8710e29797da477f879edbcceb3bc1849983a
user <user@users-MacBook-Air.local>
2013-01-19 16:56:04 -0600

    Created CLPaletteViewController.

---
+++ LoveLetter/CLPaletteViewController.h
@@ -0,0 +1,13 @@
+//
+//  CLPaletteViewController.h
+//  LoveLetter
+//
+//  Created by user on 1/19/13.
+//  Copyright (c) 2013 Kevin Lawson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CLPaletteViewController : UIViewController
+
+@end
---
+++ LoveLetter/CLPaletteViewController.m
@@ -0,0 +1,38 @@
+//
+//  CLPaletteViewController.m
+//  LoveLetter
+//
+//  Created by user on 1/19/13.
+//  Copyright (c) 2013 Kevin Lawson. All rights reserved.
+//
+
+#import "CLPaletteViewController.h"
+
+@interface CLPaletteViewController ()
+
+@end
+
+@implementation CLPaletteViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
