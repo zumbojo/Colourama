de4b0cd19da6a28484b75ef76db3a0e83c01b828
user <user@users-MacBook-Air.local>
2013-01-18 19:25:26 -0600

    Created CLPalette.

---
+++ LoveLetter/CLPalette.h
@@ -0,0 +1,13 @@
+//
+//  CLPalette.h
+//  LoveLetter
+//
+//  Created by user on 1/18/13.
+//  Copyright (c) 2013 Kevin Lawson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CLPalette : NSObject
+
+@end
---
+++ LoveLetter/CLPalette.m
@@ -0,0 +1,13 @@
+//
+//  CLPalette.m
+//  LoveLetter
+//
+//  Created by user on 1/18/13.
+//  Copyright (c) 2013 Kevin Lawson. All rights reserved.
+//
+
+#import "CLPalette.h"
+
+@implementation CLPalette
+
+@end
