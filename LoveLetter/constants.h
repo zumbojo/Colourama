6f880984875d8e7c0575de242b6cb2234f3d5cb7
user <user@users-MacBook-Air.local>
2013-01-18 20:07:44 -0600

    Added constants.h with fancy UIColor macros from another project.

---
+++ LoveLetter/constants.h
@@ -0,0 +1,17 @@
+
+// Macros:
+
+#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
+// from http://stackoverflow.com/questions/1560081/how-can-i-create-a-uicolor-from-a-hex-string
+
+// UIColorFromRGBString()
+//
+// by KML, 2012-01-06
+// based on syntax from http://stackoverflow.com/questions/2679182/have-macro-return-a-value
+// and scanner logic from http://stackoverflow.com/questions/8976148/how-to-get-an-int-in-decimal-from-a-hex-nsstring
+// requires UIColorFromRGB()
+#define UIColorFromRGBString(str) ({ \
+NSUInteger rgbHex; \
+[[NSScanner scannerWithString:str] scanHexInt:&rgbHex]; \
+UIColorFromRGB(rgbHex); \
+})
