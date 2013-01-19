e8d94966bb3653afeb747468c77a32410e843c64
user <user@users-MacBook-Air.local>
2013-01-18 19:30:25 -0600

    Created CLSlat.

---
+++ LoveLetter/CLSlat.h
@@ -0,0 +1,13 @@
+//
+//  CLSlat.h
+//  LoveLetter
+//
+//  Created by user on 1/18/13.
+//  Copyright (c) 2013 Kevin Lawson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CLSlat : NSObject
+
+@end
---
+++ LoveLetter/CLSlat.m
@@ -0,0 +1,16 @@
+//
+//  CLSlat.m
+//  LoveLetter
+//
+//  Created by user on 1/18/13.
+//  Copyright (c) 2013 Kevin Lawson. All rights reserved.
+//
+
+#import "CLSlat.h"
+
+// "A slat is a long, thin, flat piece of material." http://en.wikipedia.org/wiki/Slat
+// One or more slats comprise a CLPalette.  If I just used UIColors, I wouldn't be able to have slats of differing widths.  Hence, CLSlat.
+
+@implementation CLSlat
+
+@end
