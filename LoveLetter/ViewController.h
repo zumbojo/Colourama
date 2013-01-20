dbe8a8eaba4f3bd94da49a5a8374811efc4906ac
user <user@users-MacBook-Air.local>
2013-01-19 20:56:02 -0600

    stageView removed, starting on pageController.

--- LoveLetter/ViewController.h
+++ LoveLetter/ViewController.h
@@ -8,8 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UIPageViewControllerDataSource>
 
-@property (weak, nonatomic) IBOutlet UIView *stageView;
+@property (weak, nonatomic) UIPageViewController *pageController;
 
 @end
