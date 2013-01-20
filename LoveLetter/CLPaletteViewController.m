96abbe71f8e2c3d8eda3ff93cc6cd02852804e05
user <user@users-MacBook-Air.local>
2013-01-19 23:10:07 -0600

    Constraints logic.

--- LoveLetter/CLPaletteViewController.m
+++ LoveLetter/CLPaletteViewController.m
@@ -12,6 +12,8 @@
 
 @interface CLPaletteViewController ()
 
+@property (nonatomic) NSMutableArray *slatViews;
+
 @end
 
 @implementation CLPaletteViewController
@@ -31,6 +33,7 @@
     self = [super init];
     if (self) {
         self.palette = palette;
+        self.slatViews = [[NSMutableArray alloc] init];
     }
     return self;
 }
@@ -45,6 +48,7 @@
         UIView *slatView = [[UIView alloc] init];
         slatView.layer.backgroundColor = slat.color.CGColor;
         slatView.translatesAutoresizingMaskIntoConstraints = NO;
+        [self.slatViews addObject:slatView];
         [self.view addSubview:slatView];
         
         NSDictionary *views = NSDictionaryOfVariableBindings(slatView);
@@ -58,14 +62,24 @@
         
         // horizontal is a bit more involved:
         
-        // if first, glue left to superview left
+        if (slatView == self.slatViews[0]) { // if first, glue left to superview left
+            NSLog(@"first");
+            [self.view addConstraints:
+             [NSLayoutConstraint constraintsWithVisualFormat:@"H:|[slatView]|"
+                                                     options:0
+                                                     metrics:nil
+                                                       views:views]];
+        }
+        else { // if not first, glue left to last, set width equal to first
+            NSLog(@"not first");
+        }
         
-        // if not first, glue left to last, set width equal to first
-        
-        // if last, glue right to superview right
+        if (slatView == self.slatViews.lastObject) { // if last, glue right to superview right
+            NSLog(@"last");
+        }
     }
     
-    
+    /*
     UIView *one = [[UIView alloc] init];
     one.layer.backgroundColor = ((CLSlat *)self.palette.slats[0]).color.CGColor;
     [one setTranslatesAutoresizingMaskIntoConstraints:NO];
@@ -101,6 +115,7 @@
                                              options:0
                                              metrics:nil
                                                views:views]];
+    */
 }
 
 - (void)didReceiveMemoryWarning
