705c2ae3b2f636a3dd3f5c256625a6ca63634f7e
user <user@users-MacBook-Air.local>
2013-01-21 20:42:57 -0600

    Created CLMothership.

---
+++ LoveLetter/CLMothership.h
@@ -0,0 +1,13 @@
+//
+//  CLMothership.h
+//  LoveLetter
+//
+//  Created by user on 1/21/13.
+//  Copyright (c) 2013 Kevin Lawson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CLMothership : NSObject
+
+@end
