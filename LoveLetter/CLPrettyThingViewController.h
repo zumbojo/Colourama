3d86e1a971d3b9f8130cc4e7353af954683b39fd
user <user@users-MacBook-Air.local>
2013-01-31 18:46:03 -0600

    Created CLPrettyThingViewController.

---
+++ LoveLetter/CLPrettyThingViewController.h
@@ -0,0 +1,15 @@
+//
+//  CLPrettyThingViewController.h
+//  LoveLetter
+//
+//  Created by user on 1/31/13.
+//  Copyright (c) 2013 Kevin Lawson. All rights reserved.
+//
+
+// An Abstract class for handling things that are general to paletteVCs, colorVCs, patternVCs, etc. (such as the byline, loves/comments/views counters, etc.)
+
+#import <UIKit/UIKit.h>
+
+@interface CLPrettyThingViewController : UIViewController
+
+@end
---
+++ LoveLetter/CLPrettyThingViewController.m
@@ -0,0 +1,38 @@
+//
+//  CLPrettyThingViewController.m
+//  LoveLetter
+//
+//  Created by user on 1/31/13.
+//  Copyright (c) 2013 Kevin Lawson. All rights reserved.
+//
+
+#import "CLPrettyThingViewController.h"
+
+@interface CLPrettyThingViewController ()
+
+@end
+
+@implementation CLPrettyThingViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
