6ebf656a7bb55f5cd9fd6698b71c1202836b70ec
user <user@users-MacBook-Air.local>
2013-01-31 18:48:03 -0600

    Created CLPrettyThing, reorg.

---
+++ LoveLetter/CLPrettyThing.h
@@ -0,0 +1,13 @@
+//
+//  CLPrettyThing.h
+//  LoveLetter
+//
+//  Created by user on 1/31/13.
+//  Copyright (c) 2013 Kevin Lawson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CLPrettyThing : NSObject
+
+@end
---
+++ LoveLetter/CLPrettyThing.m
@@ -0,0 +1,13 @@
+//
+//  CLPrettyThing.m
+//  LoveLetter
+//
+//  Created by user on 1/31/13.
+//  Copyright (c) 2013 Kevin Lawson. All rights reserved.
+//
+
+#import "CLPrettyThing.h"
+
+@implementation CLPrettyThing
+
+@end
