b7852753fdf56c0d87c9fd3816984fd18127981c
user <user@users-MacBook-Air.local>
2013-02-01 19:29:10 -0600

    Created SettingsViewController.

---
+++ LoveLetter/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  LoveLetter
+//
+//  Created by user on 2/1/13.
+//  Copyright (c) 2013 Kevin Lawson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
