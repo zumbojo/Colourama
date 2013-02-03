2d42031cb476141cfd7e4b8c38fbd71738543c1d
user <user@users-MacBook-Air.local>
2013-02-02 21:42:30 -0600

    Created CLColor.

---
+++ LoveLetter/CLColor.h
@@ -0,0 +1,13 @@
+//
+//  CLColor.h
+//  LoveLetter
+//
+//  Created by user on 2/2/13.
+//  Copyright (c) 2013 Kevin Lawson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CLColor : NSObject
+
+@end
---
+++ LoveLetter/CLColor.m
@@ -0,0 +1,13 @@
+//
+//  CLColor.m
+//  LoveLetter
+//
+//  Created by user on 2/2/13.
+//  Copyright (c) 2013 Kevin Lawson. All rights reserved.
+//
+
+#import "CLColor.h"
+
+@implementation CLColor
+
+@end
