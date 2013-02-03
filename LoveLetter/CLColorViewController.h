c2c5b0d2499e90c6bec1fa23824b9631aca01356
user <user@users-MacBook-Air.local>
2013-02-02 22:38:31 -0600

    Created CLColorViewController.

---
+++ LoveLetter/CLColorViewController.h
@@ -0,0 +1,13 @@
+//
+//  CLColorViewController.h
+//  LoveLetter
+//
+//  Created by user on 2/2/13.
+//  Copyright (c) 2013 Kevin Lawson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CLColorViewController : UIViewController
+
+@end
---
+++ LoveLetter/CLColorViewController.m
@@ -0,0 +1,38 @@
+//
+//  CLColorViewController.m
+//  LoveLetter
+//
+//  Created by user on 2/2/13.
+//  Copyright (c) 2013 Kevin Lawson. All rights reserved.
+//
+
+#import "CLColorViewController.h"
+
+@interface CLColorViewController ()
+
+@end
+
+@implementation CLColorViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
