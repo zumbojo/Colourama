c3290fcb5b33f6557671d727d1a8ca873cad0a5d
user <user@users-MacBook-Air.local>
2013-02-03 20:46:25 -0600

    Mothership cleanup.

--- LoveLetter/CLMothership.h
+++ LoveLetter/CLMothership.h
@@ -8,12 +8,6 @@
 
 #import <Foundation/Foundation.h>
 
-typedef enum { // todo: deprecate
-    ColourPaletteTypeNew,
-    ColourPaletteTypeTop,
-    ColourPaletteTypeRandom
-} ColourPaletteType;
-
 typedef enum {
     CLPrettyThingVarietyNew,
     CLPrettyThingVarietyTop,
@@ -23,11 +17,7 @@ typedef enum {
 @interface CLMothership : NSObject
 
 + (CLMothership *)sharedInstance;
-
 - (void)loadPrettyThingsOfClass:(Class)prettyThingSubclass withVariety:(CLPrettyThingVariety)variety success:(void (^)(NSArray* prettyThings))success;
 - (void)loadPrettyThingsOfClass:(Class)prettyThingSubclass withVariety:(CLPrettyThingVariety)variety number:(NSUInteger)numResults offset:(NSUInteger)offset success:(void (^)(NSArray* prettyThings))success;
-- (void)loadPalettes:(void (^)(NSArray* palettes))block; // loads the top 20 palettes with default options
-- (void)loadPalettesOfType:(ColourPaletteType)type success:(void (^)(NSArray* palettes))block;
-- (void)loadPalettesOfType:(ColourPaletteType)type withNumber:(int)numResults andOffset:(int)offset success:(void (^)(NSArray* palettes))block;
 
 @end
