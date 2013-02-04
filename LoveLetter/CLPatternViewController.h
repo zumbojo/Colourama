f1e8502090a0b08aa554458bb911c728fe93b135
user <user@users-MacBook-Air.local>
2013-02-03 22:29:20 -0600

    Created CLPatternViewController.

---
+++ LoveLetter/CLPatternViewController.h
@@ -0,0 +1,13 @@
+//
+//  CLPatternViewController.h
+//  LoveLetter
+//
+//  Created by user on 2/3/13.
+//  Copyright (c) 2013 Kevin Lawson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CLPatternViewController : UIViewController
+
+@end
