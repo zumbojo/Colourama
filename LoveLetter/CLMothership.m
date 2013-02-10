178c8ee31fd10fa4c06a9fbf47ec6252ec04508e
user <user@users-MacBook-Air.local>
2013-02-10 12:23:55 -0600

    Cleanup.

--- LoveLetter/CLMothership.m
+++ LoveLetter/CLMothership.m
@@ -33,16 +33,7 @@ static const int kColourLoversDefaultPageSize = 20;
 }
 
 - (void)loadPrettyThingsOfClass:(Class)prettyThingSubclass withVariety:(CLPrettyThingVariety)variety number:(NSUInteger)numResults offset:(NSUInteger)offset success:(void (^)(NSArray* prettyThings))success {
-    
     [self loadPrettyThingsOfClasses:@[prettyThingSubclass] withVariety:variety success:success];
-    
-    
-//    NSURLRequest *request = [self requestForPrettyThingsOfClass:prettyThingSubclass withVariety:variety number:numResults offset:offset];
-//    
-//    AFJSONRequestOperation *operation = [self operationForPrettyThingsOfClass:prettyThingSubclass request:request success:success];
-//    
-//    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
-//    [queue addOperation:operation];
 }
 
 - (void)loadPrettyThingsOfClasses:(NSArray *)prettyThingSubclasses withVariety:(CLPrettyThingVariety)variety success:(void (^)(NSArray* prettyThings))success {
@@ -96,54 +87,6 @@ static const int kColourLoversDefaultPageSize = 20;
     
     NSLog(@"request pretty things at url %@",urlString);
     return [NSURLRequest requestWithURL:[NSURL URLWithString:urlString]];
-}
-
-- (AFJSONRequestOperation *)operationForPrettyThingsOfClass:(Class)prettyThingSubclass request:(NSURLRequest *)request success:(void (^)(NSArray* prettyThings))success {
-    return [AFJSONRequestOperation JSONRequestOperationWithRequest:request success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
-        NSMutableArray* parsed = [[NSMutableArray alloc] init];
-        for (id node in JSON) {
-            [parsed addObject:[[prettyThingSubclass alloc] initWithJSON:node]];
-        }
-        
-        if ([prettyThingSubclass class] == [CLPattern class]) {
-            // queue up images for download:
-            NSMutableArray *imageOperations = [[NSMutableArray alloc] init];
-            for (CLPattern* pattern in parsed) {
-                AFImageRequestOperation *imageOp = [[AFImageRequestOperation alloc] initWithRequest:[[NSURLRequest alloc] initWithURL:pattern.imageUrl]];
-                imageOp.userInfo = @{@"pattern" : pattern};
-                [imageOperations addObject:imageOp];
-            }
-            
-            // How to use "enqueueBatchOfHTTPRequestOperationsWithRequests":
-            // https://github.com/AFNetworking/AFNetworking/issues/305
-            AFHTTPClient *client = [[AFHTTPClient alloc] initWithBaseURL:[NSURL URLWithString:@""]];
-            [client enqueueBatchOfHTTPRequestOperations:imageOperations
-                                          progressBlock:^(NSUInteger numberOfCompletedOperations, NSUInteger totalNumberOfOperations) {
-                                              //NSLog(@"%d / %d", numberOfCompletedOperations, totalNumberOfOperations);
-                                          }
-                                        completionBlock:^(NSArray *operations) {
-                                            BOOL allRequestsCompletedWithoutError = true;
-                                            for (AFImageRequestOperation *ro in operations) {
-                                                if (ro.error) {
-                                                    NSLog(@"++++++++++++++ Operation error");
-                                                    allRequestsCompletedWithoutError = false;
-                                                }
-                                                else {
-                                                    ((CLPattern *)ro.userInfo[@"pattern"]).image = ro.responseImage; // assign responseImage to pattern.image
-                                                    //NSLog(@"Operation OK: %@", [ro.responseData description]);
-                                                }
-                                            }
-                                            
-                                            if (allRequestsCompletedWithoutError) {
-                                                success(parsed);
-                                            }
-                                        }];
-        }
-        else {
-            // notify the caller of our success and send the list of pretty things along
-            success(parsed);
-        }
-    } failure:nil];
 }
 
 + (CLPrettyThingJSONOperationQueueParser)parser {
